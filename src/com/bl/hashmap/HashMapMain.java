0a64cf5000dc9950f8943869bc107b8d3e2f0cd1
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-06-29 18:47:19 +0530

    added code to  add a paragraph

--- src/com/bl/hashmap/HashMapMain.java
+++ src/com/bl/hashmap/HashMapMain.java
@@ -2,20 +2,17 @@ package com.bl.hashmap;
 
 public class HashMapMain {
     public static void main(String[] args) {
-        String sentence = "to be or not to be";
-        IHashMap<String,Integer> hashMap = new HashMap<>();
-        String[] words = sentence.toLowerCase().split(" ");
-        for(String word: words) {
-            Integer value = hashMap.get(word);
-            if(value == null)
-                value = 1;
-            else
-                value = value + 1;
-            hashMap.put(word, value);
+            IHashMap<String,Integer> hashMap = new HashMap<>();
+            String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            String[] words = paragraph.toLowerCase().split(" ");
+            for(String word: words) {
+                Integer value = hashMap.get(word);
+                if(value == null)
+                    value = 1;
+                else
+                    value = value + 1;
+                hashMap.put(word, value);
+            }
+            System.out.println(hashMap);
         }
-        int frequency = hashMap.get("to");
-        System.out.println(frequency);
-        System.out.println(hashMap);
     }
-}
-
--- src/com/bl/hashmap/MapLinkedList.java
+++ src/com/bl/hashmap/MapLinkedList.java
@@ -61,4 +61,3 @@ package com.bl.hashmap;
                     '}';
         }
     }
-
